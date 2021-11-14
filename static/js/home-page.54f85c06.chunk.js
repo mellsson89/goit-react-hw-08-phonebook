e02e40f5efe63809be3d524636b0383bc98962f0
Serg <serg@MacBook-Pro-Serg.local>
2021-11-14 23:18:11 +0200

    Updates

---
+++ static/js/home-page.54f85c06.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[2],{195:function(t,o,e){"use strict";e.r(o);e(0);var n=e(2);o.default=function(){return Object(n.jsx)("h1",{children:"\u0414\u043e\u043c\u0430\u0448\u043d\u044f\u044f \u0441\u0442\u0430\u043d\u0438\u0446\u0430"})}}}]);
+//# sourceMappingURL=home-page.54f85c06.chunk.js.map
